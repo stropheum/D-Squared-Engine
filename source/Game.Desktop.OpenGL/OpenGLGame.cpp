8a287cd3a0090f2023bb2fe9f15bbc8d4af083ab
stropheum <stropheum@yahoo.com>
2017-01-11 15:26:07 -0500

    Updated warning level
    Set warning to W4, treating errors as warnings, and removed all
    warnings/errors from the code base

--- source/Game.Desktop.OpenGL/OpenGLGame.cpp
+++ source/Game.Desktop.OpenGL/OpenGLGame.cpp
@@ -3,6 +3,11 @@
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR commandLine, int showCommand)
 {
+	UNREFERENCED_PARAMETER(instance);
+	UNREFERENCED_PARAMETER(previousInstance);
+	UNREFERENCED_PARAMETER(commandLine);
+	UNREFERENCED_PARAMETER(showCommand);
+
 	SList::getData();
 	return 0;
 }
