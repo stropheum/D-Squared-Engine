9bd971fa464017e8228c897d8aeaa90169701fff
stropheum <stropheum@yahoo.com>
2017-02-03 14:10:20 -0500

    implemented find tests

--- source/Test.Library.Desktop/HashMapTest.cpp
+++ source/Test.Library.Desktop/HashMapTest.cpp
@@ -7,7 +7,7 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace TestLibraryDesktop
 {
-	TEST_CLASS(TestHashMap)
+	TEST_CLASS(HashMapTest)
 	{
 		HashMap::HashMap<int, int> intMap;
 		HashMap::HashMap<int*, int> ptrMap;
@@ -161,21 +161,118 @@ namespace TestLibraryDesktop
 		TEST_METHOD(TestAssignmentOperator)
 		{
 			// int tests
-			// ptr tests
-			// chr tests
-			// str tests
+			HashMap::HashMap<int, int> intcopy1;
+			intcopy1 = intMap;
+			Assert::IsTrue(intMap.size() == intcopy1.size(), L"HashMap sizes not equal");
+
+			intMap.insert(std::pair<int, int>(1, 1));
+			HashMap::HashMap<int, int> intcopy2;
+			intcopy2 = intMap;
+			Assert::AreEqual(intMap.size(), intcopy2.size(), L"HashMap sizes not equal");
+			Assert::IsFalse(intMap.begin() == intMap.end(), L"Begin of non-empty HashMap equals end");
+			Assert::IsFalse(intcopy2.begin() == intcopy2.end(), L"Begin of non-empty HashMap equals end");
+
+			// pointer tests
+			HashMap::HashMap<int*, int> ptrcopy1;
+			ptrcopy1 = ptrMap;
+			Assert::IsTrue(ptrMap.size() == ptrcopy1.size(), L"HashMap sizes not equal");
+
+			ptrMap.insert(std::pair<int*, int>(&x, 1));
+			HashMap::HashMap<int*, int> ptrcopy2;
+			ptrcopy2 = ptrMap;
+			Assert::AreEqual(ptrMap.size(), ptrcopy2.size(), L"HashMap sizes not equal");
+			Assert::IsFalse(ptrMap.begin() == ptrMap.end(), L"Begin of non-empty HashMap equals end");
+			Assert::IsFalse(ptrcopy2.begin() == ptrcopy2.end(), L"Begin of non-empty HashMap equals end");
+
+
+			// char* tests
+			HashMap::HashMap<char*, int> chrcopy1;
+			chrcopy1 = chrMap;
+			Assert::IsTrue(chrMap.size() == chrcopy1.size(), L"HashMap sizes not equal");
+
+			chrMap.insert(std::pair<char*, int>(a, 1));
+			HashMap::HashMap<char*, int> chrcopy2;
+			chrcopy2 = chrMap;
+			Assert::AreEqual(chrMap.size(), chrcopy2.size(), L"HashMap sizes not equal");
+			Assert::IsFalse(chrMap.begin() == chrMap.end(), L"Begin of non-empty HashMap equals end");
+			Assert::IsFalse(chrcopy2.begin() == chrcopy2.end(), L"Begin of non-empty HashMap equals end");
+
+
+			// string tests
+			HashMap::HashMap<std::string, int> strcopy1;
+			strcopy1 = strMap;
+			Assert::IsTrue(strMap.size() == strcopy1.size(), L"HashMap sizes not equal");
+
+			strMap.insert(std::pair<std::string, int>(s, 1));
+			HashMap::HashMap<std::string, int> strcopy2;
+			strcopy2 = strMap;
+			Assert::AreEqual(strMap.size(), strcopy2.size(), L"HashMap sizes not equal");
+			Assert::IsFalse(strMap.begin() == strMap.end(), L"Begin of non-empty HashMap equals end");
+			Assert::IsFalse(strcopy2.begin() == strcopy2.end(), L"Begin of non-empty HashMap equals end");
+
+
 			// foo tests
-			Assert::Fail(L"Method not implemented");
+			HashMap::HashMap<Foo, int> foocopy1;
+			foocopy1 = fooMap;
+			Assert::IsTrue(fooMap.size() == foocopy1.size(), L"HashMap sizes not equal");
+
+			fooMap.insert(std::pair<Foo, int>(foo, 1));
+			HashMap::HashMap<Foo, int> foocopy2;
+			foocopy2 = fooMap;
+			Assert::AreEqual(fooMap.size(), foocopy2.size(), L"HashMap sizes not equal");
+			Assert::IsFalse(fooMap.begin() == fooMap.end(), L"Begin of non-empty HashMap equals end");
+			Assert::IsFalse(foocopy2.begin() == foocopy2.end(), L"Begin of non-empty HashMap equals end");
 		}
 
 		TEST_METHOD(TestFind)
 		{
-			Assert::Fail(L"Method not implemented");
+			// int tests
+			Assert::IsTrue(intMap.find(1) == intMap.end(), L"Find on empty HashMap does not return end");
+			intMap.insert(std::pair<int, int>(1, 1));
+			Assert::IsTrue(intMap.find(1) == intMap.begin(), L"Only value inserted into HashMap is not begin");
+			Assert::IsFalse(intMap.find(1) == intMap.end(), L"Find on existing value should not return end");
+			Assert::AreEqual((*intMap.find(1)).second, 1, L"Find returning incorrect value");
+
+			// pointer tests
+			Assert::IsTrue(ptrMap.find(&x) == ptrMap.end(), L"Find on empty HashMap does not return end");
+			ptrMap.insert(std::pair<int*, int>(&x, 1));
+			Assert::IsTrue(ptrMap.find(&x) == ptrMap.begin(), L"Only value inserted into HashMap is not begin");
+			Assert::IsFalse(ptrMap.find(&x) == ptrMap.end(), L"Find on existing value should not return end");
+			Assert::AreEqual((*ptrMap.find(&x)).second, 1, L"Find returning incorrect value");
+
+			// char* tests
+			Assert::IsTrue(chrMap.find(c) == chrMap.end(), L"Find on empty HashMap does not return end");
+			chrMap.insert(std::pair<char*, int>(c, 1));
+			Assert::IsTrue(chrMap.find(c) == chrMap.begin(), L"Only value inserted into HashMap is not begin");
+			Assert::IsFalse(chrMap.find(c) == chrMap.end(), L"Find on existing value should not return end");
+			Assert::AreEqual((*chrMap.find(c)).second, 1, L"Find returning incorrect value");
+
+			// string tests
+			Assert::IsTrue(strMap.find(s) == strMap.end(), L"Find on empty HashMap does not return end");
+			strMap.insert(std::pair<std::string, int>(s, 1));
+			Assert::IsTrue(strMap.find(s) == strMap.begin(), L"Only value inserted into HashMap is not begin");
+			Assert::IsFalse(strMap.find(s) == strMap.end(), L"Find on existing value should not return end");
+			Assert::AreEqual((*strMap.find(s)).second, 1, L"Find returning incorrect value");
+
+			// foo tests
+			Assert::IsTrue(fooMap.find(foo) == fooMap.end(), L"Find on empty HashMap does not return end");
+			fooMap.insert(std::pair<Foo, int>(foo, 1));
+			Assert::IsTrue(fooMap.find(foo) == fooMap.begin(), L"Only value inserted into HashMap is not begin");
+			Assert::IsFalse(fooMap.find(foo) == fooMap.end(), L"Find on existing value should not return end");
+			Assert::AreEqual((*fooMap.find(foo)).second, 1, L"Find returning incorrect value");
 		}
 
 		TEST_METHOD(TestInsert)
 		{
-			Assert::Fail(L"Method not implemented");
+			// int tests
+
+			// pointer tests
+
+			// char* tests
+
+			// string tests
+
+			// foo tests
 		}
 
 		TEST_METHOD(TestRemove)
