e0d4f4dff7a0f83bc79c7aea391dce3800cb6e66
stropheum <stropheum@yahoo.com>
2017-02-14 19:10:28 -0500

    Added scope test class

---
+++ source/Test.Library.Desktop/ScopeTest.cpp
@@ -0,0 +1,50 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestLibraryDesktop
+{		
+	TEST_CLASS(ScopeTest)
+	{
+	public:
+
+		/// Sets up leak detection logic
+		static void initializeLeakDetection()
+		{
+#if _Debug
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+			_CrtMemCheckpoint(&sStartMemState);
+#endif //_Debug
+		}
+
+		/// Detects if memory state has been corrupted
+		static void finalizeLeakDetection()
+		{
+#if _Debug
+			_CrtMemState endMemState, diffMemState;
+			_CrtMemCheckpoint(&endMemState);
+			if (_CrtMemDifference(&diffMemState, &sStartMemState, &endMemState))
+			{
+				_CrtMemDumpStatistics(&diffMemState);
+				Assert::Fail(L"Memory Leaks!");
+			}
+#endif //_Debug
+		}
+
+		TEST_METHOD_INITIALIZE(methodInitialize)
+		{
+			initializeLeakDetection();
+		}
+
+		TEST_METHOD_CLEANUP(methodCleanup)
+		{
+			finalizeLeakDetection();
+		}
+
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+	};
+}
