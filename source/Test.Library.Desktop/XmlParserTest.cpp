24a6c9b89360cbba70a3322594adace69d46ae70
stropheum <stropheum@yahoo.com>
2017-03-15 20:41:55 -0400

    Added test file for xml parser

---
+++ source/Test.Library.Desktop/XmlParserTest.cpp
@@ -0,0 +1,54 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestLibraryDesktop
+{
+	TEST_CLASS(XmlParserTest)
+	{
+	public:
+
+		/// Sets up leak detection logic
+		static void initializeLeakDetection()
+		{
+#if _DEBUG
+			_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF);
+			_CrtMemCheckpoint(&sStartMemState);
+#endif //_Debug
+		}
+
+		/// Detects if memory state has been corrupted
+		static void finalizeLeakDetection()
+		{
+#if _DEBUG
+			_CrtMemState endMemState, diffMemState;
+			_CrtMemCheckpoint(&endMemState);
+			if (_CrtMemDifference(&diffMemState, &sStartMemState, &endMemState))
+			{
+				_CrtMemDumpStatistics(&diffMemState);
+				Assert::Fail(L"Memory Leaks!");
+			}
+#endif //_Debug
+		}
+
+		TEST_METHOD_INITIALIZE(methodInitialize)
+		{
+			initializeLeakDetection();
+		}
+
+		TEST_METHOD_CLEANUP(methodCleanup)
+		{
+			finalizeLeakDetection();
+		}
+
+		TEST_METHOD(TestStub)
+		{
+			Assert::Fail(L"Test not implemented");
+		}
+
+		static _CrtMemState sStartMemState;
+	};
+	_CrtMemState XmlParserTest::sStartMemState;
+}
