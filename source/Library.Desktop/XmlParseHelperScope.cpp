7df0347c6b79762614f375b74cb42678780fdba1
stropheum <stropheum@yahoo.com>
2017-03-22 21:55:55 -0400

    improved parsing. still incomplete

--- source/Library.Desktop/XmlParseHelperScope.cpp
+++ source/Library.Desktop/XmlParseHelperScope.cpp
@@ -2,6 +2,7 @@
 #include "XmlParseHelperScope.h"
 #include "SharedDataScope.h"
 #include "Scope.h"
+#include <sstream>
 
 
 namespace Library
@@ -16,7 +17,9 @@ namespace Library
 		mXmlParseMaster = xmlParseMaster;
 	}
 
-	bool XmlParseHelperScope::startElementHandler(XmlParseMaster::SharedData& sharedData, const std::string& element, const HashMap<std::string, std::string> attributes)
+	bool XmlParseHelperScope::startElementHandler(
+		XmlParseMaster::SharedData& sharedData, const std::string& element, 
+		const HashMap<std::string, std::string> attributes)
 	{
 		SharedDataScope* data = sharedData.As<SharedDataScope>();
 		if (data == nullptr) { return false; }
@@ -27,38 +30,49 @@ namespace Library
 			mState = State::ParsingInteger;
 			Datum& datum = scope->append(element);
 			datum.setType(DatumType::Integer);
-			datum.setFromString(attributes.find(element)->second);
+			datum.setFromString(attributes.find("Value")->second);
 		}
 		else if (element == "Float")
 		{
 			mState = State::ParsingFloat;
 			Datum& datum = scope->append(element);
 			datum.setType(DatumType::Float);
-			datum.setFromString(attributes.find(element)->second);
+			datum.setFromString(attributes.find("Value")->second);
 		}
 		else if (element == "Vector")
 		{
-			mState = State::ParsingVector;
+			mState = (mState == State::ParsingMatrix) ? State::ParsingMatrix : State::ParsingVector;
 			Datum& datum = scope->append(element);
 			datum.setType(DatumType::Vector);
-			datum.setFromString(attributes.find(element)->second);
+			
+			std::string x, y, z, w;
+			x = attributes.find("X")->second;
+			y = attributes.find("Y")->second;
+			z = attributes.find("Z")->second;
+			w = attributes.find("W")->second;
+			
+			std::stringstream ss;
+			ss << "vec4(" << x << "," << y << "," << z << "," << w << ")";
+			datum.setFromString(ss.str());
 		}
 		else if (element == "Matrix")
 		{
 			mState = State::ParsingMatrix;
 			Datum& datum = scope->append(element);
 			datum.setType(DatumType::Matrix);
-			datum.setFromString(attributes.find(element)->second);
+//			datum.setFromString(attributes.find(element)->second);
+			// TODO: Grab all matrix components and build a string from them
 		}
 		else if (element == "String")
 		{
 			mState = State::ParsingString;
 			Datum& datum = scope->append(element);
 			datum.setType(DatumType::String);
-			datum.set(attributes.find(element)->second);
+			datum.set(attributes.find("Value")->second);
 		}
 		else if (element == "Scope")
 		{
+			// TODO: Track scope depth and when ending scope, check if depth is greater than zero, set state back to parsing scope
 			mState = State::ParsingScope;
 			Scope& newScope = scope->appendScope(element);
 			data->mScope = &newScope; // Scope becomes the newly appended scope until we're done adding to it
@@ -74,6 +88,7 @@ namespace Library
 		
 		if (element == "Scope")
 		{	// We're done at this level, so jump up one
+			assert(data->mScope->getParent() != nullptr);
 			data->mScope = data->mScope->getParent();
 		}
 
